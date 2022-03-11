687383a77f9af73f16ab2bac59891ef7cc30f3e0
Aaron Minnick <abminnick@gmail.com>
2022-03-11 11:33:06 -0800

    move html to SlidingInfoPane

--- src/components/SlidingInfoPane.js
+++ src/components/SlidingInfoPane.js
@@ -3,7 +3,33 @@ import React from "react";
 function SildingInfoPane() {
   return (
     <React.Fragment>
-      {/*colored pane with comb top*/}
+      <div className="pane">
+        <div className="teeth">
+          <div className="tooth">
+            <div className="comb-tooth-L"></div>
+            <div className="comb-tooth-R"></div>
+          </div>
+          <div className="tooth">
+            <div className="comb-tooth-L"></div>
+            <div className="comb-tooth-R"></div>
+          </div>
+          <div className="tooth">
+            <div className="comb-tooth-L"></div>
+            <div className="comb-tooth-R"></div>
+          </div>
+          <div className="tooth">
+            <div className="comb-tooth-L"></div>
+            <div className="comb-tooth-R"></div>
+          </div>
+          <div className="tooth">
+            <div className="comb-tooth-L"></div>
+            <div className="comb-tooth-R"></div>
+          </div>
+        </div>
+        <div className="content-div">
+          <p>foo</p>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
