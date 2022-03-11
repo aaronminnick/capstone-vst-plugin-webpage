f228f50e3e6033b944ccf3fbc842e8b186c3a1b6
Aaron Minnick <abminnick@gmail.com>
2022-03-11 11:23:52 -0800

    update App.js to include child components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Header from './Header';
+import ImagePane from './ImagePane';
+import SildingInfoPane from './SlidingInfoPane';
+import Footer from './Footer';
 
 function App() {
   return (
-    <React.Fragment />
+    <React.Fragment>
+      <Header />
+      <ImagePane />
+      <SildingInfoPane />
+      <Footer />
+    </React.Fragment>
   );
 }
 
